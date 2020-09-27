d4c0c211b57c697975613a89bfa954b7f82f97c0
Andrew Herculano <andrewherculano@hotmail.com>
2020-09-27 13:30:44 -0300

    Adding CSS and JS

---
+++ UrnaDigital/etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: 12345,
+                nome: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos: [
+                    { url: '12345.jpg', legenda: 'Vereador' }
+                ]
+            },
+            {
+                numero: 67890,
+                nome: 'Beltrano da Silva',
+                partido: 'DFGH',
+                fotos: [
+                    { url: '67890.jpg', legenda: 'Vereador' }
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: 97,
+                nome: 'Ciclano',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos: [
+                    { url: '97.jpeg', legenda: 'Prefeito' },
+                    { url: '97_2.jpeg', legenda: 'Vice-Prefeito', small: true }
+                ]
+            },
+            {
+                numero: 98,
+                nome: 'Zulano',
+                partido: 'DFGH',
+                vice: 'Zul',
+                fotos: [
+                    { url: '98.jpeg', legenda: 'Prefeito' },
+                    { url: '98_2.jpeg', legenda: 'Vice-Prefeito', small: true }
+                ]
+            },
+        ]
+    }
+];
